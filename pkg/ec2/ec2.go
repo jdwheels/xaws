e3a3ed8e54e75c616e65c01c651cd277af859bad
jdwheels <Wheeler.John.D@gmail.com>
2019-10-05 15:12:44 -0400

    Debug

--- pkg/ec2/ec2.go
+++ pkg/ec2/ec2.go
@@ -2,20 +2,20 @@ package ec2
 
 import (
 	"github.com/jdwheels/xaws/internal"
+	"log"
 )
 
 func StartEC2Cluster(asgName string) bool {
-	if _, err := internal.AutoScaleIt(asgName, 1); err != nil {
-		return false
-	} else {
-		return internal.CheckIt(asgName) == nil
-	}
+	return ChangeCapacity(asgName, 1)
 }
 
 func StopEC2Cluster(asgName string) bool {
-	if _, err := internal.AutoScaleIt(asgName, 0); err != nil {
-		return false
-	} else {
-		return true
-	}
+	return ChangeCapacity(asgName, 0)
+}
+
+func ChangeCapacity(asgName string, capacity int64) bool {
+	_, err := internal.AutoScaleIt(asgName, capacity)
+	res := err != nil
+	log.Printf("Success %t", res)
+	return res
 }
